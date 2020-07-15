12bc10a67164869f110d2695c092850e88050651
Arnav Singh <66771302+Arnav-Singh-09@users.noreply.github.com>
2020-07-15 07:37:51 +0530

    Update index.php

--- PHP/index.php
+++ PHP/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Arnav Singh - Broken Rock Paper Scissors</title>
+<title>Arnav Singh - Rock Paper Scissors</title>
 <?php require_once "bootstrap.php"; ?>
 </head>
 <body>
